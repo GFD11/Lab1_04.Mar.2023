652621cffa557dee9bc4e1be8dbd4fabcf410936
GFD11 <Gerard.Francis.Donohue@students.ittralee.ie>
2023-03-06 21:43:24 +0000

    First Commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//works with mbed 5.15.2
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX);
+
+//counter
+int i=2;
+//term value
+int n=5;
+//first term
+int i_1 = 0;
+//second term
+int i_2 = 1;
+
+int main()
+{
+    pc.printf("using simple loops (%d) \r\n",n );
+    pc.printf("%d, ", i_1);
+    pc.printf("%d, ", i_2);
+
+    while (i<n) {
+        int sum = i_1 + i_2;
+        i_1 = i_2;
+        i_2=sum;
+        i++;
+        pc.printf("%d, ",sum);
+    }
+
+    while (true) {
+
+    }
+}
